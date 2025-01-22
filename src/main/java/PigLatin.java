c5608594e93b9433d9033d2bddc2f509a86f3461
Justin <justinlei471@gmail.com>
2025-01-22 09:19:41 -0800

    Update PigLatin.java

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -2,11 +2,10 @@ import java.io.File;  // Import the File class
 import java.io.FileNotFoundException;  // Import this class to handle errors
 import java.util.Scanner; // Import the Scanner class to read text files
 
-
 public class PigLatin {
-    
+
     public void tester() {
-        //String[] lines = loadStrings("words.txt");
+        // String[] lines = loadStrings("words.txt");
         String[] lines = new String[8]; 
         try{
             File myFile = new File("words.txt");
